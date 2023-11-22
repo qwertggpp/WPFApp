a23bc38f20d9f5d420b48cccf37221ea8b542727
KichiginaInna <inna291002@mail.ru>
2023-11-22 16:58:07 +0500

    Добавьте файлы проекта.

---
+++ 20.11WPF/AddEditPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20._11WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selctedHotel)
+        {
+            InitializeComponent();
+            if (selctedHotel != null)
+                _currentHotel = selctedHotel;
+
+
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = ToursBaseEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+           
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars <1 || _currentHotel.CountOfStars >5)
+                errors.AppendLine("Количество звёзд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.Id == 0)
+                ToursBaseEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                ToursBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void ComboCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 20.11WPF/ToursPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20._11WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для ToursPage.xaml
+    /// </summary>
+    public partial class ToursPage : Page
+    {
+        public ToursPage()
+        {
+            InitializeComponent();
+            var allTypes = ToursBaseEntities.GetContext().Type.ToList();
+            allTypes.Insert(0, new Type
+            {
+                Name = "Все типы"
+            });
+            ComboType.ItemsSource = allTypes;
+            CheckActual.IsChecked = true;
+            ComboType.SelectedIndex = 0;
+            UpdateTours();
+        }
+
+        private void UpdateTours()
+        {
+            var currentTours = ToursBaseEntities.GetContext().Tour.ToList();
+
+            if (ComboType.SelectedIndex > 0)
+                currentTours = currentTours.Where(p => p.Type.Contains(ComboType.SelectedItem as Type)).ToList();
+
+            currentTours = currentTours.Where(p => p.Name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (CheckActual.IsChecked.Value)
+                currentTours = currentTours.Where(p => p.IsActual).ToList();
+
+            LViewTours.ItemsSource = currentTours.OrderBy(p => p.TicketCount).ToList();
+        }
+        private void CheckActual_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void TBoxSearch_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+    }
+}
